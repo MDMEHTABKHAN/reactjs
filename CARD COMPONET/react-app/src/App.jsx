b4db96fd49bde392619e51f4acbcb1fb68a83b13
Md Mehtab Khan <109038370+MDMEHTABKHAN@users.noreply.github.com>
2024-10-27 23:53:57 +0530

    Add files via upload

---
+++ CARD COMPONET/react-app/src/App.jsx
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+import Header from './Header';
+import Footer from './Footer';
+import Foot from './Foot';
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Header/>
+     <Footer/>
+     <Foot/>
+=======
+import Card from "./Card"
+
+function App() {
+  
+  return (
+    <>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+
+>>>>>>> 0a3402f (first commit)
+    </>
+  )
+}
+
+export default App
